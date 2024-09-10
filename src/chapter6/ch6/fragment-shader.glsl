d8271fbce838d6af04e0420fe16d7c19ca208be8
J█████ <dev.nullinside@gmail.com>
2024-09-10 12:09:06 -0400

    baseline

---
+++ src/chapter6/ch6/fragment-shader.glsl
@@ -0,0 +1,10 @@
+﻿#version 330 core
+out vec4 FragColor;
+
+in vec4 vertexColor; // input variable from vs (same name and type)
+uniform vec4 ourColor; // we set this variable in the OpenGL code.
+
+void main() {
+    // FragColor = vertexColor;  // Using the output from the vertex-shader.glsl
+    FragColor = ourColor;   // Using the uniform
+}
